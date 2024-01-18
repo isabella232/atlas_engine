f0e7c7f1906bce0d7c6757c01fd6f1616c99ca9c
Keith Wiersema <keith.wiersema@shopify.com>
2024-01-18 11:17:34 -0600

    revert locale specific country profile

--- app/models/atlas_engine/address_validation/normalizer.rb
+++ app/models/atlas_engine/address_validation/normalizer.rb
@@ -18,6 +18,15 @@ module AtlasEngine
           .gsub("æ", "ae")
           .gsub("œ", "oe")
           .gsub("  ", " ")
+          # normalizes Arabic characters
+          .tr("آ", "ا")
+          .tr("أ", "ا")
+          .tr("إ", "ا")
+          .tr("ئ", "ي")
+          .tr("ة", "ه")
+          .tr("ى", "ي")
+          # removes Arabic stretching characters and diacritics
+          .gsub(/[\u064B|\u064C|\u064D|\u064E|\u064F|\u0650|\u0651|\u0652|\u0640]/, "")
           # TODO: Strip hyphens for USPS not zip
           .gsub(/[!@%&"'*,.();:]/, "")
           .downcase
--- test/models/atlas_engine/address_validation/normalizer_test.rb
+++ test/models/atlas_engine/address_validation/normalizer_test.rb
@@ -10,10 +10,16 @@ module AtlasEngine
         extend Normalizer
       end
 
-      test "#normalize returns a normalized string" do
-        string = "Æ Œ æ œ!@%&\"'*,.();:"
-        expected_normalized_string = "ae oe ae oe"
-        assert_equal expected_normalized_string, TestNormalizer.normalize(string)
+      test "#normalize returns a normalized address" do
+        arabic_city_strings = {
+          "الطائف" => "الطايف",
+          "قرية" => "قريه",
+          "رحــــــيم" => "رحيم",
+        }
+
+        arabic_city_strings.each do |city_string, expected_normalized_string|
+          assert_equal expected_normalized_string, TestNormalizer.normalize(city_string)
+        end
       end
     end
   end
