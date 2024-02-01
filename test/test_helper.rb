27fd37508db0913ecfaa854d7f62c34676c42ce9
Dominique Flabbi <dominique.flabbi@shopify.com>
2024-02-01 15:40:40 -0500

    fix fixture_path deprecation warning

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -19,10 +19,11 @@ ActiveRecord::Migrator.migrations_paths = [File.expand_path("../test/dummy/db/mi
 ActiveRecord::Migrator.migrations_paths << File.expand_path("../db/migrate", __dir__)
 
 # Load fixtures from the engine
-if ActiveSupport::TestCase.respond_to?(:fixture_path=)
-  ActiveSupport::TestCase.fixture_path = File.expand_path("fixtures", __dir__)
-  ActionDispatch::IntegrationTest.fixture_path = ActiveSupport::TestCase.fixture_path
-  ActiveSupport::TestCase.file_fixture_path = ActiveSupport::TestCase.fixture_path + "/files"
+if ActiveSupport::TestCase.respond_to?(:fixture_paths=)
+  fixture_path = File.expand_path("fixtures", __dir__)
+  ActiveSupport::TestCase.fixture_paths = [fixture_path]
+  ActionDispatch::IntegrationTest.fixture_paths = ActiveSupport::TestCase.fixture_paths
+  ActiveSupport::TestCase.file_fixture_path = [fixture_path + "/files"]
   ActiveSupport::TestCase.fixtures(:all)
 end
 
